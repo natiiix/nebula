c8cf87401b014c32188e4ba7b343d8f86aa1971d
natiiix <natiwastaken@gmail.com>
2018-12-02 19:57:52 +0100

    Switch from BIOS printing to VGA text buffer printing
    Based on code from: https://wiki.osdev.org/Babystep4

--- boot.asm
+++ boot.asm
@@ -1,36 +1,62 @@
-%macro print 1
+%macro PRINT 1
     mov si, %1
-    call bios_print
+    call print_str
 %endmacro
 
-[ORG 0x7C00]
+[ORG 0x7C00]            ; boot sector memory address
 
-    cld             ; lowest-to-highest address string direction
+    cli                 ; clear interrupt flag
+    cld                 ; lowest-to-highest byte string direction
 
-    xor ax, ax      ; ax = 0
-    mov ds, ax      ; ds = ax
+    xor ax, ax          ; AX = 0
+    mov ds, ax          ; DS = 0
+    mov ss, ax          ; stack top = 0
+    mov sp, 0x9C00      ; stack bottom = 0x2000 behind the beginning of the code
 
-    print msg
+    mov ax, 0xB800      ; VGA text buffer address
+    mov es, ax
+
+    PRINT msg           ; print string
 
 hang:
-    jmp hang        ; infinite hang loop
+    jmp hang            ; infinite hang loop
 
-bios_print:
-    lodsb           ; al = *si++
-    or al, al
-    jz return       ; al == '\0'
+print_str_char:
+    call print_char     ; print single character
+print_str:
+    lodsb               ; load next string character / byte
+    cmp al, 0           ; check for null string termination character
+    jne print_str_char  ; string printing loop
 
-    mov ah, 0x0E
-    mov bh, 0x00
-    int 0x10        ; BIOS print character interrupt
+    mov byte [xpos], 0  ; carriage return
+    add byte [ypos], 1  ; line feed
 
-    jmp bios_print  ; string printing loop
-
-return:             ; label used for conditional return
     ret
 
-msg     db 'Hello World!', 13, 10, 0
+print_char:
+    mov ah, 0x0F        ; attribute byte - white on black
+    mov cx, ax          ; store character and attribute in CX
+    movzx ax, byte [ypos]
+    mov dx, 160         ; 160 bytes per line (80 columns, 2 bytes per column / character)
+    mul dx              ; multiply Y position by number of bytes per line
+    movzx bx, byte [xpos]
+    shl bx, 1           ; take X position and multiply it by 2 to skip attributes
 
-	times 510-($-$$) db 0
+    mov di, 0           ; start of video memory
+    add di, ax          ; add y offset
+    add di, bx          ; add x offset
+
+    mov ax, cx          ; restore char/attribute
+    stosw               ; write char/attribute
+    add byte [xpos], 1  ; advance to right
+
+    ret
+
+xpos    db 0
+ypos    db 0
+
+msg     db "Hello World!", 0
+
+    times 510-($-$$) db 0
     db 0x55
-    db 0xAA         ; boot sector signature
+    db 0xAA
