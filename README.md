aeaa075e1ac515607d1d17ad706ada0dc073e6ca
natiiix <natiwastaken@gmail.com>
2018-12-08 23:42:03 +0100

    Add bootloader code to allow more than 510 bytes of code
    Added code is heavily based on Floppy Bird bootloader by Mihail Szabolcs
    ( https://github.com/icebreaker/floppybird/blob/master/src/boot.asm )

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Nebula
+
+## Used foreign code
+
+- [Floppy Bird bootloader](https://github.com/icebreaker/floppybird/blob/master/src/boot.asm) ([LICENSE](LICENSES/icebreaker_floppybird))
--- boot.asm
+++ boot.asm
@@ -16,10 +16,90 @@
     call finish_print
 %endmacro
 
+%define SECTORS 16							; keep it under 18
+%define IMAGE_SIZE ((SECTORS + 1) * 512)	; SECTORS + 1 (~= 18) * 512 bytes
+%define STACK_SIZE 256						; 4096 bytes in paragraphs
+
 [BITS 16]               ; 16-bit instructions
 [ORG 0x7C00]            ; boot sector memory address
 
-    jmp kernel_init
+start:
+	cli ; disable interrupts
+
+	;
+	; Notes:
+	;  1 paragraph	= 16 bytes
+	; 32 paragraphs = 512 bytes
+	;
+	; Skip past our SECTORS
+	; Skip past our reserved video memory buffer (for double buffering)
+	; Skip past allocated STACK_SIZE
+	;
+	mov ax, (((SECTORS + 1) * 32) + 4000 + STACK_SIZE)
+	mov ss, ax
+	mov sp, STACK_SIZE * 16 ; 4096 in bytes
+
+	sti ; enable interrupts
+
+	mov ax, 07C0h			; point all segments to _start
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+
+	; dl contains the drive number
+
+	mov ax, 0				; reset disk function
+	int 13h					; call BIOS interrupt
+	jc fatal
+
+	; FIXME: if SECTORS + 1 > 18 (~= max sectors per track)
+	; then we should try to do _multiple_ reads
+	;
+	; Notes:
+	;
+	; 1 sector			= 512 bytes
+	; 1 cylinder/track	= 18 sectors
+	; 1 side			= 80 cylinders/tracks
+	; 1 disk (1'44 MB)	= 2 sides
+	;
+	; 2 * 80 * 18 * 512 = 1474560 bytes = 1440 kilo bytes = 1.4 mega bytes
+	;
+	; We start _reading_ at SECTOR 2 because SECTOR 1 is where our stage 1
+	; _bootloader_ (this piece of code up until the dw 0xAA55 marker, if you
+	; take the time and scroll down below) is *loaded* automatically by BIOS
+	; and therefore there is no need to read it again ...
+
+	push es				; save es
+
+	mov ax, 07E0h		; destination location (address of _start)
+	mov es, ax			; destination location
+	mov bx, 0			; index 0
+
+	mov ah, 2			; read sectors function
+	mov al, SECTORS		; number of sectors
+	mov ch, 0			; cylinder number
+	mov dh, 0			; head number
+	mov cl, 2			; starting sector number
+	int 13h				; call BIOS interrupt
+
+	jc fatal
+
+	pop es				; restore es
+
+	jmp 07E0h:0000h		; jump to _start (a.k.a stage 2)
+
+fatal:
+	mov ax, 0	; wait for a keypress
+	int 16h
+
+	mov ax, 0	; reboot
+	int 19h
+
+times 510-($-$$) db 0   ; pad with zeroes to fill first segment (bootloader segment)
+    dw 0xAA55           ; terminate first sector with bootloader sector signature
+
+    jmp kernel_init     ; entry point (beginning of second segment)
 
 gdt_start:
 
@@ -296,6 +376,4 @@ hexaddr dw 0
 
 msg     db "Hello World!", 0
 
-    times 510-($-$$) db 0
-    db 0x55
-    db 0xAA
+times IMAGE_SIZE-($-$$) db 0    ; pad with zeroes to fill IMAGE_SIZE
