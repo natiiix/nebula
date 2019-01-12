3dad222f3d954e3ae0c599ae3dcf21da82fb167d
natiiix <natiwastaken@gmail.com>
2019-01-12 23:17:26 +0100

    Move 16-bit initialization code (protected mode switch) to init16.asm

--- src/main.asm
+++ src/main.asm
@@ -11,26 +11,7 @@
     call finish_print
 %endmacro
 
-[BITS 16]               ; 16-bit instructions
-[ORG 0x7C00]            ; boot sector memory address
-
-%include "bootloader.asm"
-
-kernel_init:            ; kernel initialization (enter protected mode)
-    cli
-
-    mov eax, 0
-    call fill_segments  ; set up segment registers
-
-    mov sp, 0xFFFC      ; set 16-bit stack pointer
-
-    lgdt [gdt_desc]     ; load GDT
-
-    mov eax, cr0        ; enable bit 0 of CR0
-    or eax, 0b1
-    mov cr0, eax
-
-    jmp CODE_SEG:init32 ; jump to protected mode code to finish initialization
+%include "init16.asm"
 
 [BITS 32]               ; 32-bit instructions
 
