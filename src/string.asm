3634ab874e2dfdfc966a138c9a4a6d7c2966075f
natiiix <natiwastaken@gmail.com>
2019-01-14 01:37:33 +0100

    Optimize strlen to avoid modifying register with string address
    I did not realize it was possible to add two registers inline like this

--- src/string.asm
+++ src/string.asm
@@ -1,17 +1,14 @@
 ; @desc Determines the length of a string.
 ; @in   ESI Memory address of the string to evaluate.
 ; @out  ECX Length of evaluated string.
-; @out  ESI Index of string-terminating null character.
 strlen:
     mov ecx, 0          ; set string length counter to zero
 
 strlen_loop:
-    cmp byte [esi], 0   ; check for terminating null character
+    cmp byte [esi + ecx], 0   ; check for terminating null character
     je strlen_done
 
-    inc ecx             ; increment string length counter
-    inc esi             ; move on to next string character
-
+    inc ecx             ; increment string length counter / character index
     jmp strlen_loop     ; loop until null character is encountered
 
 strlen_done:
