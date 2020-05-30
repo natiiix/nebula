78630a0854bbd27e605d2433acad938746d4c866
Ivo Meixner <natiwastaken@gmail.com>
2020-05-30 03:25:23 +0200

    Improve benchmark macros

--- src/macro/benchmark.asm
+++ src/macro/benchmark.asm
@@ -1,8 +1,21 @@
 %macro BENCHMARK_BEGIN 0
-    .bench:
+%if BENCHMARK_MODE_ENABLED
+.bench:
+%endif
 %endmacro
 
 %macro BENCHMARK_END 0
-    adc dword [benchmark_count], 0
+%if BENCHMARK_MODE_ENABLED
+    inc dword [benchmark_count]
     jmp .bench
+%endif
+%endmacro
+
+; Constant macro used to toggle the benchmark mode.
+; When the benchmark mode is enabled, the benchmark code runs
+; in an infinite loop instead of jumping to the shell loop.
+BENCHMARK_MODE_ENABLED equ 0
+
+%macro BENCHMARK_CODE 0
+    ; <<< code to be benchmarked >>>
 %endmacro
