8cac5208026131643a3b742df7cedbc0acf596dc
Ivo Meixner <natiwastaken@gmail.com>
2020-06-01 03:01:50 +0200

    Add Out-of-Memory error message to heap data

--- src/data/heap.asm
+++ src/data/heap.asm
@@ -1,7 +1,9 @@
 SECTION .data
 
 primary_heap_tab    times 0x100 dd 0    ; the top-level heap memory table has 256 32-bit entries
+primary_heap_tab_end:
 
 SECTION .rodata
 
-err_invalid_size db "Requested allocation size is invalid (zero or too large).", 0
+err_invalid_size    db "Requested allocation size is invalid (zero or too large).", 0
+err_out_of_memory   db "Out of memory (unable to find enough continuous memory block for allocation).", 0
