ee91e3ac4a492ce310ea1be269471a55ca724a96
Ivo Meixner <natiwastaken@gmail.com>
2020-06-03 03:17:48 +0200

    Implement logic for finding secondary tables

--- src/heap.asm
+++ src/heap.asm
@@ -3,25 +3,34 @@ SECTION .text
 ; Rounded-up division by 256.
 ; Used to get the number of blocks necessary for a heap allocation.
 %macro SHR8_ROUND_UP 0
-    add eax, 0xFF
-    shr eax, 8
+    add edx, 0xFF
+    shr edx, 8
+%endmacro
+
+%macro DEBUG_PRINT 1
+    pusha
+    mov eax, %1
+    call print32
+    popa
 %endmacro
 
 ; @in   EAX Requested memory block size.
 ; @out  ESI Address of allocated memory space.
 malloc:
+    mov edx, eax
+
     SHR8_ROUND_UP       ; divide by 256 and round up to get the number of tertiary blocks
     ; JC could be used here to handle attempts to allocate more than 0xFFFFFF00 bytes.
-    test eax, eax       ; check if the number of tertiary blocks to allocate
+    test edx, edx       ; check if the number of tertiary blocks to allocate
     jz .invalid_size    ; if it is zero, either 0 or more than 0xFFFFFF00 bytes have been requested
 
     ; At this point, EAX should be in the 0x00000001 - 0x00FFFFFF range.
 
-    test eax, ~0xFF ; check higher bits (above least significant byte)
+    test edx, ~0xFF ; check higher bits (above least significant byte)
     jz .tertiary    ; zero => below 256 => requested memory size can fit into the tertiary allocation level
 
     SHR8_ROUND_UP   ; this makes EAX < 0x10000 => number of secondary blocks
-    test eax, ~0xFF ; requested size can fit into secondary block(s)
+    test edx, ~0xFF ; requested size can fit into secondary block(s)
     jz .secondary
 
     SHR8_ROUND_UP   ; this makes EAX < 0x100 => number of primary blocks
@@ -31,11 +40,11 @@ malloc:
     PRINTLN err_invalid_size
     ret
 
-; @in   EAX Number of requested primary blocks.
+; @in   EDX Number of requested primary blocks.
 ; @out  ESI Address of table entry of the first available block.
 ; @reg  EAX, ECX, EDX
 .primary:
-    mov edx, eax                    ; EAX will be used by string instructions
+    ; mov edx, eax                    ; EAX will be used by string instructions
 
     ; Check if there are enough primary memory blocks.
     ; This does not mean that there are really enough continuous blocks.
@@ -97,6 +106,14 @@ malloc:
     ;               - Propagate value into primary table.
     ;           - Not found.
     ;               - Find a different secondary table.
+
+    call find_secondary_table
+    test esi, esi
+    jz .secondary_create
+    ret
+
+.secondary_create:
+    DEBUG_PRINT 0xBAD0_CCCC
     mov esi, 0
     ret
 
@@ -112,40 +129,75 @@ malloc:
 
 ; @in   EDX Required number of unused entries.
 find_secondary_table:
-    mov esi, primary_heap_tab + 4
+    mov esi, primary_heap_tab + 4   ; start at the second entry in primary table
 
-.find_loop:
+.find_table_loop:
+    cmp esi, primary_heap_tab_end
+    jae .fail
+
     lodsd
     test eax, 0xFF00_0000           ; check the most significant byte to determine if this is a nested table entry
     jnz .found
 
     test eax, eax                   ; check if the table entry is used
-    jz .find_loop_end               ; zero => unused primary table entry
+    jz .find_table_loop             ; zero => unused primary table entry
 
     ; and eax, 0xFF                 ; not necessary, we know from the previous check that this is not a nested table block
     dec eax                         ; LODSD has already incremented the entry pointer
     shl eax, 2                      ; convert from index into 32-bit entry offset
     add esi, eax                    ; skip the allocated data block entries
 
-.find_loop_end:
-    cmp esi, primary_heap_tab_end
-    jb .find_loop
-
-    mov esi, 0
-    ret
+    jmp .find_table_loop
 
 .found:
+    DEBUG_PRINT 0xBAD0_FFFF
     mov ebx, 0xFF                   ; maximal number of blocks that can be allocated
     shr eax, 24                     ; most significant byte contains number of already allocated sub-blocks in a nested table
     sub ebx, eax                    ; calculated number of unused blocks
     cmp ebx, edx                    ; check if there is enough space in the table for the requested allocation
-    jb .find_loop                   ; definitely not enough space in the table
+    jb .find_table_loop             ; definitely not enough space in the table
+
+    mov edi, esi                    ; save pointer to unused primary entry for later
 
     sub esi, primary_heap_tab + 4   ; caculate primary table entry offset (+4 because ESI was already incremented to the next table entry)
     shl esi, 24 - 2                 ; get the address of the secondary table
+
+    DEBUG_PRINT esi
+
+    add esi, 4                      ; move to second entry
+    mov ebx, esi                    ; set EBX to address of end of this secondary table
+    add ebx, 0xFF * 4               ; skip 255 32-bit table entries to get to the end of the table
+
+    DEBUG_PRINT esi
+    DEBUG_PRINT ebx
+
+.find_entry_loop:
+    DEBUG_PRINT 0xBAD0_BBBB
+    call find_unused_32b_entry
+    test esi, esi
+    jz .unsuitable_table            ; this secondary table cannot be used, find a different one
+
+    call count_zero_32b
+    cmp ecx, edx
+    jb .find_entry_loop             ; not enough unused secondary entries in a row
+
+    shl ecx, 2                      ; convert count into 32-bit entry offset
+    sub esi, ecx                    ; calculate address of first zero entry in secondary table
+    ret
+
+.unsuitable_table:
+    DEBUG_PRINT 0xBAD0_DDDD
+    mov esi, edi                    ; restore address of entry in primary table
+    ; add esi, 4                      ; move to the next entry in primary table
+    jmp .find_table_loop
+
+.fail:
+    mov esi, 0                      ; there is no suitable secondary table => return NULL pointer
     ret
 
 ; Finds the first unused entry in the primary table and returns a pointer to it.
+; @out  ESI Address of unused table entry (or NULL if the table is already fully used).
+; @reg  EAX, EBX
 find_unused_primary_entry:
     mov esi, primary_heap_tab + 4
     mov ebx, primary_heap_tab_end
@@ -168,6 +220,10 @@ find_unused_32b_entry:
     ret
 
 .next_entry:
+    DEBUG_PRINT 0xEEEE_EEEE
+    DEBUG_PRINT esi
+    DEBUG_PRINT eax
+
     and eax, 0xFF       ; only the least significant byte is used to number of allocated bytes (more significant bytes are reserved)
     dec eax             ; decrement table entry locally because LODSD has already moved ESI to the next entry
     shl eax, 2          ; convert table entry value into table offset (32-bit entries)
@@ -231,14 +287,16 @@ ret_esi_null:
 ; @in   ESI Start address.
 ; @in   EBX End address (exclusive).
 ; @out  ECX Number of consecutive 32-bit zeros.
+; @out  ESI Address of first non-zero 32-bit value or end address.
 count_zero_32b:
-    COUNT_ZERO lodsd eax
+    COUNT_ZERO lodsd, eax
 
 ; @in   ESI Start address.
 ; @in   EBX End address (exclusive).
 ; @out  ECX Number of consecutive 8-bit zeros.
+; @out  ESI Address of first non-zero 8-bit value or end address.
 count_zero_8b:
-    COUNT_ZERO lodsb al
+    COUNT_ZERO lodsb, al
 
 ; Clears the specified number of 4-byte memory blocks starting at address in EDI.
 %macro CLEAR_4BYTE 1
